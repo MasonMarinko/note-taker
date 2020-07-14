b15633566ec91bbb03b6031cc7476bd3b4f60cac
MasonM <Mason.P.Marinko@gmail.com>
2020-07-13 18:54:16 -0600

    Adding to Github, project working, now working on ReadME and deploying to Heroku

--- library/notesindex.js
+++ library/notesindex.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 
 function addNote(body, notesArray) {
     notesArray.push(body);
-    
+
     fs.writeFileSync(
         path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
+        JSON.stringify({
+            notes: notesArray
+        }, null, 2)
     );
     return addNote;
 }
@@ -14,22 +16,25 @@ function addNote(body, notesArray) {
 function idChecker(noteId, array) {
     array.forEach((note, index) => {
         if (note.id === noteId) {
-            console.log(`noteDeleting index: ${index}`);
-            serverDeleteNote(index, array);
+            deleteNote(index, array);
         }
     })
 }
 
-function serverDeleteNote(index, array) {
-    console.log(`array before: ${array}`);
+function deleteNote(index, array) {
     array.splice(index, 1);
-    console.log(`array after: ${array}`);
 
     fs.writeFileSync(
         path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: array }, null, 2)
+        JSON.stringify({
+            notes: array
+        }, null, 2)
     );
     return array;
 }
 
-module.exports = { addNote, serverDeleteNote, idChecker }
+module.exports = {
+    addNote,
+    deleteNote,
+    idChecker
+}
--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
-const { addNote, idChecker } = require('./library/notesindex');
+const {
+    notes
+} = require('./db/db.json');
+const {
+    addNote,
+    idChecker
+} = require('./library/notesindex');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -18,14 +25,12 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    console.log(notes)
     res.json(notes);
 });
 
+// add note and apply id based on length of array (just like in module)
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-    // console.log(newNote)
-    console.log(notes)
     const newNote = addNote(req.body, notes);
 
     res.json(newNote)
@@ -34,7 +39,10 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res) => {
     noteId = req.params.id;
     idChecker(noteId, notes);
-    res.json({ message: 'success', data: req.body})
+    res.json({
+        message: 'success',
+        data: req.body
+    })
 })
 
 app.listen(PORT, () => {
